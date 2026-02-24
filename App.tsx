71bc7020b5075b92981fb790965f50724722e470
SevvalOcal <148449976+SevvalOcal@users.noreply.github.com>
2026-02-24 22:47:51 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Şevval Öcal</p>
+      <p>Ogrenci No: 235541014</p>
+      <p>Bolum: Yazilim Muhendisligi</p>
+      <p>Hobi: Kod yazmak</p>
+    </div>
+  )
+}
+
+export default App
