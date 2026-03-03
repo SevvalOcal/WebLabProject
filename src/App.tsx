4e91cd2c79a9ed479148403b2b508bb5527ab2be
sevval <sevval.ocl@gmail.com>
2026-03-03 15:59:27 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <p><strong>Ad Soyad:</strong> Şevval Öcal</p>
+      <p><strong>Öğrenci No:</strong> 235541014</p>
+    </div>
+  )
+}
+export default App
